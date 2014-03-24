93860921389b92357fe8122e72259a46fde6e800
unknown <mcdonamp@MCDONAMP-1.rose-hulman.edu>
2014-03-24 00:55:52 -0400

    Finished intro android project

--- workspace/HelloWorld/src/edu/rosehulman/helloworld/MainActivity.java
+++ workspace/HelloWorld/src/edu/rosehulman/helloworld/MainActivity.java
@@ -1,15 +1,29 @@
 package edu.rosehulman.helloworld;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+	public final static String EXTRA_MESSAGE = "edu.rosehulman.helloworld.MESSAGE";
+	
+	private EditText mEditText;
+	private Button mSendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        this.mEditText = (EditText)findViewById(R.id.edit_message);
+        this.mSendButton = (Button)findViewById(R.id.send_button);
+        this.mSendButton.setOnClickListener(this);
     }
 
 
@@ -19,5 +33,23 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.send_button:
+			Intent intent = new Intent(this, DisplayMessageActivity.class);
+			String message = this.mEditText.getText().toString();
+			intent.putExtra(EXTRA_MESSAGE, message);
+			startActivity(intent);
+			break;
+
+		default:
+			Log.d("ME435", "We should never have gotten here because there are no other buttons to click!");
+			break;
+		}
+		
+	}
     
 }
